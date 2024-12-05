de2938d0bc5c0057e8f7aae6f3023b2728920b75
Peter Weingartner <pweingar@gmail.com>
2024-12-05 15:54:02 -0500

    Added bitmap to the _Vicky structure

--- include/f256/vicky.h
+++ include/f256/vicky.h
@@ -15,78 +15,6 @@ typedef struct color {
 // Flat video ram address 0 - 256K
 typedef uint8_t __far24 *vram_ptr;
 
-// ----------------------------------------------------------------------
-//
-// Vicky master control
-//
-// ----------------------------------------------------------------------
-
-struct _Vicky {
-  union {                                 // Master control
-    struct {
-      uint8_t text_enable         : 1;
-      uint8_t text_overlay_enable : 1;
-      uint8_t graphics_enable     : 1;
-      uint8_t bitmap_enable       : 1;
-      uint8_t tilemap_enable      : 1;
-      uint8_t sprites_enbale      : 1;
-      uint8_t gamma_enable        : 1;
-
-      uint8_t resolution          : 1;
-      uint8_t double_pixel_x      : 1;    // text mode only
-      uint8_t double_pixel_y      : 1;    // text mode only
-      uint8_t                     : 5;
-    };
-    uint16_t  master_control;
-  };
-  struct {                                // Layer manager
-    uint8_t layer0 : 3;
-    uint8_t        : 1;
-    uint8_t layer1 : 3;
-    uint8_t        : 1;
-  };
-  struct {
-    uint8_t layer2 : 3;
-    uint8_t        : 5;
-  };
-  struct {                                // Border control
-    uint8_t border_enable : 1;
-    uint8_t               : 3;
-    uint8_t border_scroll : 3;
-    uint8_t               : 1;
-  };
-  color_t border_color;
-  uint8_t border_width;                   // 0 - 31
-  uint8_t border_height;                  // 0 - 31
-  uint8_t _reserved0;
-  uint8_t _reserved1;
-  uint8_t _reserved2;
-  color_t background_color;               // Background color
-  struct {                                // Cursor control
-    uint8_t cursor_enable       : 1;
-    uint8_t cursor_flash_rate   : 2;
-    uint8_t cursor_flash_enable : 1;
-  };
-  uint8_t _reserved3;
-  char cursor_char;
-  uint8_t _reserved4;
-  uint16_t cursor_x;
-  uint16_t cursor_y;
-  union {
-    struct {                              // Write: Line interrupt
-      struct {
-        uint8_t line_interrupt_enable : 1;
-      };
-      uint16_t line_interrupt_line;
-      uint8_t _reserved5;
-    };
-    struct {                              // Read: Pixel/line position
-      uint16_t pixel_position;            // 0 - 799 (includes blanking)
-      uint16_t active_line;               // 0 - 525 / 0 - 449 (includes blanking)
-    };
-  };
-};
-
 // Resolution enums
 enum VickyTextResolution {
   Text_640x480_60Hz, Text_640x480_70Hz
@@ -181,6 +109,82 @@ typedef struct sprite {
 
 enum sprite_size {
   Sprite_32x32, Sprite_24x24, Sprite_16x16, Sprite_8x8
+};
+
+// ----------------------------------------------------------------------
+//
+// Vicky master control
+//
+// ----------------------------------------------------------------------
+
+struct _Vicky {
+  union {                                 // Master control
+    struct {
+      uint8_t text_enable         : 1;
+      uint8_t text_overlay_enable : 1;
+      uint8_t graphics_enable     : 1;
+      uint8_t bitmap_enable       : 1;
+      uint8_t tilemap_enable      : 1;
+      uint8_t sprites_enbale      : 1;
+      uint8_t gamma_enable        : 1;
+
+      uint8_t resolution          : 1;
+      uint8_t double_pixel_x      : 1;    // text mode only
+      uint8_t double_pixel_y      : 1;    // text mode only
+      uint8_t                     : 5;
+    };
+    uint16_t  master_control;
+  };
+  struct {                                // Layer manager
+    uint8_t layer0 : 3;
+    uint8_t        : 1;
+    uint8_t layer1 : 3;
+    uint8_t        : 1;
+  };
+  struct {
+    uint8_t layer2 : 3;
+    uint8_t        : 5;
+  };
+  struct {                                // Border control
+    uint8_t border_enable : 1;
+    uint8_t               : 3;
+    uint8_t border_scroll : 3;
+    uint8_t               : 1;
+  };
+  color_t border_color;
+  uint8_t border_width;                   // 0 - 31
+  uint8_t border_height;                  // 0 - 31
+  uint8_t _reserved0;
+  uint8_t _reserved1;
+  uint8_t _reserved2;
+  color_t background_color;               // Background color
+  struct {                                // Cursor control
+    uint8_t cursor_enable       : 1;
+    uint8_t cursor_flash_rate   : 2;
+    uint8_t cursor_flash_enable : 1;
+  };
+  uint8_t _reserved3;
+  char cursor_char;
+  uint8_t _reserved4;
+  uint16_t cursor_x;
+  uint16_t cursor_y;
+  union {
+    struct {                              // Write: Line interrupt
+      struct {
+        uint8_t line_interrupt_enable : 1;
+      };
+      uint16_t line_interrupt_line;
+      uint8_t _reserved5;
+    };
+    struct {                              // Read: Pixel/line position
+      uint16_t pixel_position;            // 0 - 799 (includes blanking)
+      uint16_t active_line;               // 0 - 525 / 0 - 449 (includes blanking)
+    };
+  };
+
+  uint8_t _reserve6[228];			      // Skip to the bitmap registers
+
+  bitmap_t bitmap[3];
 };
 
 
